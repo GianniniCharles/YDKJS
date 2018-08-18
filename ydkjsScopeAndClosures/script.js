efbd54e559aa081b305f5eb40608f5f7976ebd39
Giannini Charles <12gcharles@gmail.com>
2018-08-18 18:55:50 -0400

    Daily github update

---
+++ ydkjsScopeAndClosures/script.js
@@ -0,0 +1,19 @@
+//LHS Lookup: Assignment:
+
+let a = 2;
+
+//RHS lookup: grab the value of the variable in the scope:
+
+console.log(a);
+
+/*Today, 8/18/18, I helped Kenneth Solano work in the Chrome
+DevTools to run prompts, assign a value to a variable, and
+create an object in line with You Don't Know JS: Up & Going.
+
+I also am helping a senior dev write code for a hybrid app
+within a private repository that will not show up on Github,
+because we're using a private repo on bitbucket.
+
+
+
+*/
